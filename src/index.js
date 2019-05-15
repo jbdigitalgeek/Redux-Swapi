badbcf3156e533c5fa8efdc70de20cf749abdaa5
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-05-15 12:54:28 -0700

    initial commit starting refactor

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  applyMiddleware
 );
 
 ReactDOM.render(
